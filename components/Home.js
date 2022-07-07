cf9a76eaa8568989402919773bdcf88208387163
PrajwalTS-Blackbuck <prajwal.ts@blackbuck.com>
2022-07-07 16:49:25 +0530

    api call

--- components/Home.js
+++ components/Home.js
@@ -1,19 +1,34 @@
 import { Text, View, Pressable, StyleSheet } from "react-native";
 import Food from "./Food";
 import { food } from "../constant";
+import { useEffect, useState } from "react";
 
 const HomeScreen = ({ navigation }) => {
+  const [foodList, setFoodList] = useState([]);
+
   const onPressFunction = () => {
     navigation.navigate("Profile");
   };
+
+  const getFoodList = async () => {
+    const data = await fetch("http://demo3671678.mockable.io/food").then(
+      (response) => response.json()
+    );
+    setFoodList(data);
+  };
+
+  useEffect(() => {
+    getFoodList();
+  }, []);
 
   return (
     <View style={styles.container}>
       <Pressable onPress={onPressFunction} style={styles.button}>
         <Text style={styles.buttonText}>Go to Profile</Text>
       </Pressable>
-      {food.map((foodItem) => (
-        <Food foodItem={foodItem} />
+      {foodList.length === 0 && <Text>No food items available</Text>}
+      {foodList.map((foodItem) => (
+        <Food foodItem={foodItem} key={foodItem.id} />
       ))}
     </View>
   );
